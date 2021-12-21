abbf5d8faa08e48382fd065bcde82d08ecabb068
lym6692575 <82617535@qq.com>
2021-12-21 21:33:53 +0800

    1221-1

--- src/main/java/com/dad/wiki/WikiApplication.java
+++ src/main/java/com/dad/wiki/WikiApplication.java
@@ -8,6 +8,7 @@ public class WikiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WikiApplication.class, args);
+		System.out.println("惊呆了老铁，这是什么表演，从来没见过，算是让我开了眼！ \n");
 	}
 
 }

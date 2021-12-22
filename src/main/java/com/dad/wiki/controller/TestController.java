a56637a3467450ef064d0b0e9fa7a6d415ccc57b
lym6692575 <82617535@qq.com>
2021-12-22 21:16:51 +0800

    1222-1

---
+++ src/main/java/com/dad/wiki/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.dad.wiki.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello world!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World,Post," + name;
+    }
+}

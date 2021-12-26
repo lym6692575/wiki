a16df41d016242230135c788191597aff4a201b7
lym6692575 <82617535@qq.com>
2021-12-26 09:39:14 +0800

    1226-2 前端跨域问题

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -61,11 +61,15 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import axios from 'axios'
 
 export default defineComponent({
   name: "Home",
-  components: {
-    // HelloWorld,
+  setup() {
+    console.log("setup")
+    axios.get("http://localhost:8800/ebook/list?name=Spring").then((response) =>{
+      console.log(response)
+    }) 
   },
 });
 </script>

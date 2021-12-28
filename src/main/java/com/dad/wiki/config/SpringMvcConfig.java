9aae6331e44a390d02904736fdf170da67b4630e
lym6692575 <82617535@qq.com>
2021-12-28 20:19:21 +0800

    1228-1 增加AOP 打印接口好事，注释掉过滤器和拦截器

--- src/main/java/com/dad/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/dad/wiki/config/SpringMvcConfig.java
@@ -1,20 +1,20 @@
-package com.dad.wiki.config;
-
-import com.dad.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor LogInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(LogInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+//package com.dad.wiki.config;
+//
+//import com.dad.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor LogInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(LogInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}

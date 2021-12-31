61a563f10295e15838a91f4c007b98166743d260
lym6692575 <82617535@qq.com>
2021-12-31 15:49:03 +0800

    1231-1 修改了首页的axios参数

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -98,19 +98,19 @@ import {
 } from "@ant-design/icons-vue";
 import axios from "axios";
 
-const listData: Record<string, string>[] = [];
+// const listData: Record<string, string>[] = [];
 
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: "https://www.antdv.com/",
-    title: `ant design vue part ${i}`,
-    avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    description:
-      "Ant Design, a design language for background applications, is refined by Ant UED Team.",
-    content:
-      "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
-  });
-}
+// for (let i = 0; i < 23; i++) {
+//   listData.push({
+//     href: "https://www.antdv.com/",
+//     title: `ant design vue part ${i}`,
+//     avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+//     description:
+//       "Ant Design, a design language for background applications, is refined by Ant UED Team.",
+//     content:
+//       "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+//   });
+// }
 
 export default defineComponent({
   name: "Home",
@@ -124,11 +124,13 @@ export default defineComponent({
     const ebooks = ref();
     const ebooks1 = reactive({ books: [] });
     onMounted(() => {
-      axios.get("/ebook/list").then((response) => {
-        const data = response.data;
-        ebooks.value = data.content;
-        ebooks1.books = data.content;
-      });
+      axios
+        .get("/ebook/list", { params: { page: 1, size: 1000 } })
+        .then((response) => {
+          const data = response.data;
+          ebooks.value = data.content.list;
+          // ebooks1.books = data.content;
+        });
     });
     const pagination = {
       onChange: (page: number) => {
@@ -144,7 +146,7 @@ export default defineComponent({
     return {
       ebooks,
       ebooks2: toRef(ebooks1, "books"),
-      listData,
+      // listData,
       pagination,
       actions,
     };

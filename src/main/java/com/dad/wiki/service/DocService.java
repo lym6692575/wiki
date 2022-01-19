64ccf49338d55f5608604c826573301be19ff441
lym6692575 <82617535@qq.com>
2022-01-19 18:50:59 +0800

    20220119-1 增加读取文档内容功能

--- src/main/java/com/dad/wiki/service/DocService.java
+++ src/main/java/com/dad/wiki/service/DocService.java
@@ -121,4 +121,9 @@ public class DocService {
         criteria.andIdIn(ids);
         docMapper.deleteByExample(docExample);
     }
+
+    public String findContent(Long id) {
+        Content content = contentMapper.selectByPrimaryKey(id);
+        return content.getContent();
+    }
 }
